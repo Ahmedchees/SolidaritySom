a846f7278c6b2551ac4c8209d3e3da8c46f574d9
unknown <axmedjs@gmail.com>
2026-03-26 16:24:13 +0300

    Version 7.0

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,14 +4,15 @@
             title: "Clean Water Project Transforms Local Community",
             excerpt: "Our recent water rehabilitation project has provided safe drinking water to over 3,000 people...",
             description: "Access to clean water is a fundamental human right. Through our partnership with local authorities, we rehabilitated 5 deep wells and installed solar-powered pumps. This initiative now serves more than 3,000 individuals daily, drastically reducing waterborne diseases and improving livelihoods. Women and children no longer walk long distances, and families can focus on farming and education.",
-            datePublished: "March 12, 2026",
+            datePublished: "September 01, 2025",
             imageMain: "assets/images/programs/water_comunity.jpeg",
             gallery: [
                 "assets/images/programs/water_comunity_2.jpeg",
                 "assets/images/programs/water_comunity_3.jpeg",
                 "assets/images/programs/water_comunity_4.jpeg",
                 "assets/images/programs/water_comunity_5.jpeg",
-                "assets/images/programs/water_comunity_6.jpeg"
+                "assets/images/programs/water_comunity_6.jpeg",
+                "assets/images/programs/water_comunity_7.jpeg"
             ],
             videoUrl: "assets/videos/water-project.mp4",
             hasVideo: false,
@@ -33,43 +34,47 @@
         },
         {
             id: 3,
-            title: "Empowering Youth Through Education Programs",
-            excerpt: "Our youth empowerment initiatives are helping young people gain skills and confidence...",
-            description: "Education opens doors. This year, 450 young people (60% girls) graduated from our vocational training program in Kismayo. Skills learned include carpentry, ICT, and renewable energy installation. Graduates received startup kits, and many are now self-employed. We also renovated 4 classrooms and provided school supplies to 2,000+ children in remote villages.",
-            datePublished: "January 20, 2026",
-            imageMain: "assets/images/blog/blog3.jpg",
+            title: "FGM TRAINING FOR COMMUNITY ELDERS",
+            excerpt: "Fgm training for community elders and civil society and police forces in kismayo...",
+            description: "This training equips community elders with the religious and health-based arguments for abandonment, provides police forces with the legal framework to enforce Jubaland’s anti-FGM laws, and empowers civil society organizations to lead sustainable social dialogue. Through this coordinated approach, we transform FGM from a private tradition into a public health and human rights priority",
+            datePublished: "January 15, 2025",
+            imageMain: "assets/images/programs/eldersTraining.jpeg",
             gallery: [
-                "assets/images/blog/edu1.jpg",
-                "assets/images/blog/edu2.jpg",
-                "assets/images/blog/edu3.jpg"
+                "assets/images/programs/eldersTraining.jpeg",
+                "assets/images/programs/eldersTraining1.jpeg"
             ],
-            videoUrl: "assets/videos/youth-empowerment.mp4",
+            videoUrl: "assets/images/programs/elders.mp4",
             hasVideo: true
         },
         {
             id: 4,
-            title: "Emergency Health Campaign Reaches Remote Areas",
-            excerpt: "Mobile health units provided maternal care and vaccinations to over 2,500 people...",
-            description: "In partnership with UNFPA, our mobile clinics visited hard-to-reach villages, offering prenatal checkups, vaccinations, and nutrition screening. More than 2,500 individuals received life-saving care. We also trained 30 community health workers to promote hygiene and family planning. This campaign reduced malnutrition rates by 18% in targeted locations.",
-            datePublished: "March 5, 2026",
-            imageMain: "assets/images/blog/health1.jpg",
+            title: "Villages consultation meeting ",
+            excerpt: "Villages consultation meeting for the opening of new project in buulo gaduud...",
+            description: "Community and other stake holders including soldiers in frontline villages consultation meeting for the opening of new project in buulo gaduud of River rine community of kismayo district.",
+            datePublished: "Augost 9, 2025",
+            imageMain: "assets/images/programs/villageconsultation.jpeg",
             gallery: [
-                "assets/images/blog/health2.jpg",
-                "assets/images/blog/health3.jpg"
+                "assets/images/programs/villageconsultation1.jpeg",
+               "assets/images/programs/villageconsultation2.jpeg",
+               "assets/images/programs/villageconsultation3.jpeg",
+               "assets/images/programs/villageconsultation4.jpeg",
+               "assets/images/programs/villageconsultation5.jpeg"
             ],
             videoUrl: "",
             hasVideo: false
         },
         {
             id: 5,
-            title: "New Sanitation Facilities in Kismayo Schools",
-            excerpt: "Rehabilitation of 12 school toilets improved hygiene for 4,000 students...",
-            description: "Safe sanitation is key for children's education. We reconstructed gender-separated latrines and handwashing stations in 12 schools across Kismayo. Hygiene promotion sessions reached 4,000 students, reducing absenteeism due to illness. Teachers report higher attendance rates, especially among girls.",
-            datePublished: "February 10, 2026",
-            imageMain: "assets/images/blog/sanitation1.jpg",
+            title: "Buulo Gaduud Integrated Riverine Protection & Water Access Project.",
+            excerpt: "Safeguarding Buulo Gaduud: Flood Resilience and Clean Water Security....",
+            description: "Located along the banks of the Juba River, Buulo Gaduud faces the recurring threat of seasonal flooding, which frequently destroys livelihoods and contaminates vital water sources. This initiative by SolidaritySom Agency focuses on a two-pillar strategy for community safety: Flood Defense & Riverine Protection: Construction and reinforcement of river embankments and nature-based levees to prevent overflow into residential and agricultural areas. This protects the village from displacement and crop loss during high-flow seasons. Shallow Well Rehabilitation & Access: Protecting existing shallow wells from flood-water contamination by raising well-heads and installing sanitary seals. This ensures that even during heavy rains, the people of Buulo Gaduud have uninterrupted access to safe, potable water.",
+            datePublished: "April 03, 2025",
+            imageMain: "assets/images/programs/bulagudud_flood.jpeg",
             gallery: [
-                "assets/images/blog/san2.jpg",
-                "assets/images/blog/san3.jpg"
+                "assets/images/programs/bulagudud_flood1.jpeg",
+               "assets/images/programs/bulagudud_flood2.jpeg",
+               "assets/images/programs/bulagudud_flood3.jpeg",
+               "assets/images/programs/bulagudud_flood4.jpeg",
             ],
             videoUrl: "",
             hasVideo: false
